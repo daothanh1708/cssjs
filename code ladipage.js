9c992da25924ee0fdd1e32056939810d98bd7a49
daothanh1708 <73976638+daothanh1708@users.noreply.github.com>
2020-11-11 08:55:34 +0700

    Add files via upload

---
+++ code ladipage.js
@@ -0,0 +1,48 @@
+<link href="https://raw.githack.com/linhfaceseo/cssjs/master/phongring.css" rel="stylesheet" type="text/css">
+
+
+<div class="fix_tel">
+	    <div class="ring-alo-phone ring-alo-green ring-alo-show" id="ring-alo-phoneIcon" style="right: 150px; bottom: -12px;">
+	        <div class="ring-alo-ph-circle"></div>
+	        <div class="ring-alo-ph-circle-fill"></div>
+	        <div class="ring-alo-ph-img-circle">
+
+	          <a href="tel:0931342229" class="tel1">
+	            <img class="lazy callbody tel1" src="https://tratinhtam.com/wp-content/uploads/2019/10/nut-goi.png" alt="<php _e('Click to Call','call-now'); ?>" data-l="" scale="0">
+	          
+	            </a>
+	        </div>
+	      </div>
+	      <div class="tel">
+	          <p class="fone">0906311919</p>
+	    </div>
+</div>
+
+
+
+<link href="https://raw.githack.com/linhfaceseo/cssjs/master/toolbar.css" rel="stylesheet"><div id="toolbar">
+        <ul>
+                <li>
+                        <a href="tel:0938114119" title="" class="callfooter">
+                                <img class="callfooter" alt="" src="https://1.bp.blogspot.com/-GQMu1P-MKwQ/XXI_CQpGheI/AAAAAAAAAXk/IP0Epk0XAgkrUZZoGlQqmLZP38b3TzLlACLcBGAs/s1600/phone.png"> <br>Gọi điện
+                        </a>
+                </li>
+                <li>
+                    
+                        <a href="sms:0938114119;?&amp;body=Tôi quan tâm dự án Gem Sky World. Tư vấn giúp tôi." title="">
+                                <img alt="" src="https://1.bp.blogspot.com/-TjuOFlwG7RQ/XXI_CINawsI/AAAAAAAAAXc/kWlqJ17ZK_0lGYAUgrX1L4YmdX_hgTN5QCLcBGAs/s1600/message.png"> <br>Nhắn Tin
+                        </a>
+                </li>
+                <li>
+                        <a href="https://zalo.me/0938114119" title="">
+                                <img alt="" src="https://1.bp.blogspot.com/-NQOpvhlIeYE/XXI_C1IV9yI/AAAAAAAAAXo/jvcQ9dL5qTMIFrFubduMB6gZxS01TJlxQCLcBGAs/s1600/zalo.png"> <br>Chat zalo
+                        </a>
+                        </li>
+                <li>
+                        <a href="https://www.messenger.com/t/112078120563143" title="">
+                                <img alt="" src="https://1.bp.blogspot.com/-SN6kpH5e1N4/XXI_CVlV19I/AAAAAAAAAXg/vvI1ru_tk80YJoBBwvrDn12Q7_UNm--LgCLcBGAs/s1600/facebook.png"> <br>Chat facebook
+                        </a>
+                </li>
+                <div class="clearfix">
+        </div></ul>
+</div>
