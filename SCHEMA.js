9c3b8df12ef08945f8afd455f4de85f4121e646b
ECOE Homes <73976638+daothanh1708@users.noreply.github.com>
2021-09-13 17:23:37 +0700

    Add files via upload

---
+++ SCHEMA.js
@@ -0,0 +1,76 @@
+
+<script type="application/ld+json">{
+	"@context": "http://schema.org",
+  	"@type": "RealEstateAgent",
+"@id":"https://ecoehomes.com/",
+	"url": "https://ecoehomes.com/",
+	"logo": "https://ecoehomes.com/wp-content/uploads/2020/11/ecohomes-logo.png",
+    "image":"https://ecoehomes.com/wp-content/uploads/2020/11/ecohomes-logo.png",
+    "priceRange":"1000$-30000$",
+	"hasMap": "https://www.google.com/maps/place/ECOE+Homes/@10.8151843,106.6733303,17z/data=!4m6!3m5!1s0x31752546df14c4a1:0x47a9660a9d117779!8m2!3d10.783142!4d106.778935!15sCh1lY29lIGhvbWVzIDEzNCBi4bqhY2ggxJHhurFuZ5IBEGNvcnBvcmF0ZV9vZmZpY2U?shorturl=1",	
+	"email": "mailto:daothanh1708@gmail.com",
+    "founder": "Đào Trung Thành",
+  	"address": {
+    	"@type": "PostalAddress",
+    	"addressLocality": "Tân Bình",
+        "addressCountry": "VIỆT NAM",
+    	"addressRegion": "Hồ Chí Minh",
+    	"postalCode":"700000",
+    	"streetAddress": "134 Đ. Bạch Đằng, Phường 2, Tân Bình, Thành phố Hồ Chí Minh"
+  	},
+  	"description": "ECOE Homes - Chuyên trang thông tin Bất Động Sản online tại TP.HCM. Với tôn chỉ xuyên suốt: Tâm - Tầm - Tài - Tiền. ECOE Homes cam kết sẽ nỗ lực không ngừng để mang đến cho Quý khách hàng những thông tin Chính xác, Nhanh chóng và Tốt nhất về thị trường bất động sản TP.HCM và các tỉnh lân cận. Đồng thời cung cấp những thông tin cần thiết như thông tin thị trường, kiến thức pháp lý, các nhận định từ chuyên gia....để có thể luôn là cánh tay phải đắc lực trong hành trình đầu tư bất động sản của Quý khách.",
+	"name": "ECOE Homes",
+  	"telephone": "0947-000-039",
+  	"openingHours": "Mo-Su allday",
+  	"geo": {
+    	"@type": "GeoCoordinates",
+   	"latitude": "10.8151843",
+    	"longitude": "106.6733303"
+ 		}, 			
+         "potentialAction": {
+    "@type": "ReserveAction",
+    "target": {
+      "@type": "EntryPoint",
+      "urlTemplate": "https://ecoehomes.com/lien-he/",
+      "inLanguage": "vn",
+      "actionPlatform": [
+        "http://schema.org/DesktopWebPlatform",
+        "http://schema.org/IOSPlatform",
+        "http://schema.org/AndroidPlatform"
+      ]
+    },
+    "result": {
+      "@type": "Reservation",
+      "name": "Liên hệ"
+    }
+  },
+ 
+  	"sameAs" : [ "https://www.facebook.com/ecoehomesgroup",
+    	"https://www.youtube.com/channel/UCo1WJcu8REVeOJfCs_vHcNA",
+		"https://www.instagram.com/ecoehomes",
+		"https://www.linkedin.com/in/ecoehomes/",
+		"https://www.flickr.com/people/ecoehomes/",
+		"https://www.behance.net/ecoehomes",
+		"https://www.twitch.tv/ecoehomes",
+		"https://www.pinterest.com/ecoehomes",
+        "https://www.tumblr.com/blog/ecoehomes"]
+	}</script>
+	<script type="application/ld+json">{
+  "@context": "http://schema.org",
+  "@type": "Person",
+  "name": "Đào Trung Thành",
+  "jobTitle": "CEO",
+  "image" : "https://ecoehomes.com/wp-content/uploads/2021/08/AVATAR.jpg",
+   "worksFor" : "ECOE Homes",
+  "url": "https://www.facebook.com/daothanh1708",
+"sameAs":["https://www.facebook.com/daothanh1708",
+"https://twitter.com/DaoTrungThanh3",
+"https://www.youtube.com/channel/UCo1WJcu8REVeOJfCs_vHcNA",
+"https://github.com/daothanh1708" ], 
+"AlumniOf" : [ "Trường Trung học phổ thông Trần Hưng Đạo",
+"Ho Chi Minh City University of Technology and Education" ],
+"address": {
+  "@type": "PostalAddress",
+    "addressLocality": "Ho Chi Minh",
+    "addressRegion": "vietnam"
+	 }}</script>
